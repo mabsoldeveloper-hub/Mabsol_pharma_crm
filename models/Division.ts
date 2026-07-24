699734bd338ab55d0f230c67c00cbf517cdeea68
salim782 <salimhusain782@gmail.com>
2026-07-24 11:16:32 +0530

    division creationfgfddgg

--- models/Division.ts
+++ models/Division.ts
@@ -1,11 +1,87 @@
+// import mongoose, { Schema, models, model } from "mongoose";
+
+// const DivisionSchema = new Schema(
+//   {
+//     companyId: {
+//       type: Schema.Types.ObjectId,
+//       ref: "Company",
+//       required: true,
+//     },
+
+//     divisionCode: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//       uppercase: true,
+//     },
+
+//     divisionName: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+
+//     shortName: {
+//       type: String,
+//       trim: true,
+//       default: "",
+//     },
+
+//     description: {
+//       type: String,
+//       trim: true,
+//       default: "",
+//     },
+
+//     status: {
+//       type: String,
+//       enum: ["Active", "Inactive"],
+//       default: "Active",
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// // Same company me duplicate division code allow nahi hoga
+// DivisionSchema.index(
+//   {
+//     companyId: 1,
+//     divisionCode: 1,
+//   },
+//   {
+//     unique: true,
+//   }
+// );
+
+// // Same company me duplicate division name bhi allow nahi hoga
+// DivisionSchema.index(
+//   {
+//     companyId: 1,
+//     divisionName: 1,
+//   },
+//   {
+//     unique: true,
+//   }
+// );
+
+// export default models.Division || model("Division", DivisionSchema);
+
 import mongoose, { Schema, models, model } from "mongoose";
 
 const DivisionSchema = new Schema(
   {
-    companyId: {
-      type: Schema.Types.ObjectId,
-      ref: "Company",
+    companyCode: {
+      type: String,
       required: true,
+      trim: true,
+    },
+
+    companyName: {
+      type: String,
+      required: true,
+      trim: true,
     },
 
     divisionCode: {
@@ -23,14 +99,14 @@ const DivisionSchema = new Schema(
 
     shortName: {
       type: String,
-      trim: true,
       default: "",
+      trim: true,
     },
 
     description: {
       type: String,
-      trim: true,
       default: "",
+      trim: true,
     },
 
     status: {
@@ -44,22 +120,11 @@ const DivisionSchema = new Schema(
   }
 );
 
-// Same company me duplicate division code allow nahi hoga
+// Same Company me duplicate Division Code allow nahi
 DivisionSchema.index(
   {
-    companyId: 1,
+    companyCode: 1,
     divisionCode: 1,
-  },
-  {
-    unique: true,
-  }
-);
-
-// Same company me duplicate division name bhi allow nahi hoga
-DivisionSchema.index(
-  {
-    companyId: 1,
-    divisionName: 1,
   },
   {
     unique: true,
