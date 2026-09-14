a9b184e1ed2025eb0a542403e27b206f733ec211
salim782 <salimhusain782@gmail.com>
2026-09-14 17:54:30 +0530

    mr section

--- models/User.ts
+++ models/User.ts
@@ -38,6 +38,15 @@ const UserSchema = new mongoose.Schema(
       default: "MR",
     },
 
+    // Original Customer record for MR / Field Staff mapping
+    mrCustomerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Customer",
+      default: null,
+      unique: true,
+      sparse: true,
+    },
+
     reportsTo: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
